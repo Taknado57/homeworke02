daa3174d56be339aeea45d3ae7d8676409f25300
Baku Baku <you@example.com>
2023-07-08 16:29:31 +0600

    дом задание02

---
+++ dom02/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Number = int.Parse(Console.ReadLine());
+string NumberTop = Convert.ToString(Number);
+if (NumberTop.Length > 2)
+{
+  Console.WriteLine("третья цифра -> " + NumberTop[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
